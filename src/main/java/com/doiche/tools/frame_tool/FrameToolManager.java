080c0de969f3cef54d24a92ab035fcaefc68fe85
zchan1325 <3dollee@gmail.com>
2022-11-17 20:51:10 +0900

    managers add

--- src/main/java/com/doiche/tools/frame_tool/FrameToolManager.java
+++ src/main/java/com/doiche/tools/frame_tool/FrameToolManager.java
@@ -1,7 +1,9 @@
 package com.doiche.tools.frame_tool;
 
+import com.doiche.utils.Scheduler;
 import com.doiche.worlds.WorldManager;
 import org.bukkit.Location;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import oshi.util.tuples.Pair;
 
@@ -12,6 +14,8 @@ public class FrameToolManager {
     private static final Map<Player, Pair<Location, Location>> locations = new HashMap<>();
     private static final Location ZERO = new Location(WorldManager.OVERWORLD,0.0f,0.0f,0.0f);
     private static final Pair<Location, Location> PAIR = new Pair<>(ZERO, ZERO);
+    private static final Particle particle = Particle.DRIPPING_DRIPSTONE_LAVA;
+    private static final int count = 2;
 
     public static void registerPlayer(Player player){
         if(!locations.containsKey(player)) locations.put(player, PAIR);
@@ -25,4 +29,11 @@ public class FrameToolManager {
         return locations.get(player);
     }
 
+    public static void showFrame(Player player){
+        Scheduler.async(() -> {
+            for(int i = 0; i < 16; i++){
+                player.getWorld().spawnParticle(particle, player.getLocation(), count);
+            }
+        });
+    }
 }
